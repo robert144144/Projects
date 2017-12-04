464593f33f8d0ce791f888b3a2cab76b62e16ee1
Robert Seth <sethr@rpi.edu>
2017-12-04 13:22:36 -0500

    Lots of stuff added

---
+++ Spellcard Soccer 2D/Assets/Scripts for Test Lvl/AttackStick.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackStick : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject myWeaponPrefab; 
+	public Animator anim;
+	private float _Timer;
+	private float _Cooldown;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (_Timer > 0.0f)
+		{
+			_Timer = Mathf.Max(0.0f, _Timer - Time.deltaTime);
+		}
+
+		if (Input.GetKey (KeyCode.Z) && _Timer <= 0.0f) {
+			anim.SetBool ("Attack", true);
+			_Timer = _Cooldown;
+		} else {
+			anim.SetBool ("Attack", false);
+			_Timer = _Cooldown;
+		}
+	}
+}
---
+++ Spellcard Soccer 2D/Assets/Scripts for Test Lvl/NaturalMovementPlatforming.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NaturalMovementPlatforming : MonoBehaviour
+{
+	[Header("Movement")]
+	[SerializeField]
+	private float _acceleration;
+	[SerializeField]
+	private float _maxAcceleration;
+	[SerializeField]
+	private float _maxVelocity;
+
+	[Header("Jumping")]
+	[SerializeField]
+	private float _groundedDistance;
+	[SerializeField]
+	private float _jumpVelocity;
+	[SerializeField]
+	private float _jumpCooldown;
+
+	private Rigidbody _rigidbody3d;
+	private float _jumpTimer;
+
+	private void Awake()
+	{
+		_rigidbody3d = GetComponent<Rigidbody>();
+	}
+	private void Update()
+	{
+		_rigidbody3d.AddForce(
+			NaturalMovement.MatchVelocityForce(
+				new Vector3(
+					Input.GetAxis("Horizontal"),
+					0.0f,
+					Input.GetAxis("Vertical")) * _maxVelocity,
+				new Vector3(_rigidbody3d.velocity.x, 0.0f, _rigidbody3d.velocity.z),
+				_acceleration,
+				_maxAcceleration));
+
+		// Jumping
+		if (
+			Input.GetKeyDown(KeyCode.Space) &&
+			_rigidbody3d.velocity.y == 0)
+		{
+			_rigidbody3d.velocity = new Vector3(_rigidbody3d.velocity.x, _jumpVelocity, _rigidbody3d.velocity.z);
+		}
+
+		// Falling 
+		if (_rigidbody3d.velocity.y < 0) {
+			_rigidbody3d.velocity += Vector3.up * Physics.gravity.y * 1.5f * Time.deltaTime;
+		}
+			
+	}
+	private void FixedUpdate()
+	{
+		_rigidbody3d.velocity = new Vector3(
+			NaturalMovement.CapVelocity(new Vector3(_rigidbody3d.velocity.x, 0.0f, _rigidbody3d.velocity.z), _maxVelocity).x,
+			_rigidbody3d.velocity.y, NaturalMovement.CapVelocity(new Vector3(_rigidbody3d.velocity.x, 0.0f, _rigidbody3d.velocity.z), _maxVelocity).z);
+	}
+}
